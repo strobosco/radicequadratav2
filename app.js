ec93bb27b6431f8c6bc70c1ad669602ae59074f1
Niccolo <niccolonobili@gmail.com>
2021-02-06 23:12:31 +0100

    Served index.html

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+    res.sendFile(path.join(__dirname + "/html/index.html"));
+});
+
 
 app.listen(PORT, () => {
     console.log("App listening on:${PORT}");
